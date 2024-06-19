b99a4cc894a3d1bc18a61a52eb37e8e369117df7
anushap6571 <146144960+anushap6571@users.noreply.github.com>
2024-06-19 18:01:15 -0500

    everything working

--- README.md
+++ README.md
@@ -8,10 +8,3 @@ enter time and sound plays when the time is met
 $ python3 -m venv /path/to/project/name_of_env
 $ source /path/to/project/name_of_env/bin/activate
 ```
-
-# install playsound
-
-```
-$ pip install --upgrade setuptools wheel
-$ pip install playsound
-```
--- main.py
+++ main.py
@@ -1,36 +1,30 @@
 import time
-import os
-from playsound import playsound
+import subprocess
 
 
 def main():
     time_w_colon = input("Enter the time: ")
 
     times = time_w_colon.split(":")
-    print(times[0])
-    print(times[1])
     target_hour = int(times[0])
     target_minute = int(times[1])
 
     now = time.localtime()
     
-    hour = now.tm_hour%12
+    hour = (now.tm_hour)%12
     minute = now.tm_min
-    print(hour)
-    print(minute)
+
     stop = False
     #playsound("/Applications/python_projects/alarm/radar.mp3")
     while not stop:
         now = time.localtime()
-        hour = now.tm_hour
+        hour = now.tm_hour%12
         minute = now.tm_min
-        print("check")
-        print(type(minute))
-        print(type(target_minute))
-        if hour == target_hour and minute == target_minute:
+
+        if (hour == target_hour) and (minute == target_minute):
             stop = True
             print("alarm")
-            #playsound("/Applications/python_projects/alarm/radar.mp3")
+            subprocess.call(["afplay", "/Applications/python_projects/alarm/radar.mp3"])
             break
         time.sleep(30)
         
